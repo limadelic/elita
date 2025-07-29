890678804fbccad251d9a9bb0f9f786b9ddaae23
msuarz <mikemps@gmail.com>
2025-07-29 01:40:23 -0400

    wip greed

--- test/elita_tester.exs
+++ test/elita_tester.exs
@@ -2,8 +2,7 @@ defmodule ElitaTester do
   import ExUnit.Assertions
   import Node, only: [set_cookie: 1]
   import Elita, only: [start_link: 1]
-  import GenServer, only: [call: 2]
-  import Enum, only: [each: 2]
+  import GenServer, only: [call: 3]
 
   def start agent do
     Node.start :"test@127.0.0.1"
@@ -11,13 +10,11 @@ defmodule ElitaTester do
     start_link agent
   end
 
-  def verify(agent, q, a) when is_binary(a) do
-    response = call {:global, agent}, {:act, q}
+  def verify agent, a, q do
+    response = call {:global, agent}, {:act, q}, 30000
+    IO.puts "Q: #{q}"
+    IO.puts "A: #{response}"
     assert String.contains? response, a
   end
 
-  def verify(agent, q, a) when is_list(a) do
-    response = call {:global, agent}, {:act, q}
-    each a, &assert(String.contains? response, &1)
-  end
 end
