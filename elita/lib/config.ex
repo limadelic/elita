8201d63100b748eeb3d82aa34e6ed31362ef64a7
msuarz <mikemps@gmail.com>
2025-07-29 00:03:14 -0400

    remove duplicate elita subfolder

--- elita/lib/config.ex
+++
@@ -1,8 +0,0 @@
-defmodule AgentConfig do
-  def config(name) do
-    case File.read("agents/#{name}.md") do
-      {:ok, content} -> content
-      {:error, _} -> "You are a #{name} agent."
-    end
-  end
-end
