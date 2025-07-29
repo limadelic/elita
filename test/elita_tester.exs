8e55370251982295bd45f10020b2543d40ad4054
msuarz <mikemps@gmail.com>
2025-07-29 00:26:10 -0400

    cleanup

--- test/elita_tester.exs
+++ test/elita_tester.exs
@@ -1,15 +1,23 @@
 defmodule ElitaTester do
   import ExUnit.Assertions
+  import Node, only: [set_cookie: 1]
+  import Elita, only: [start_link: 1]
+  import GenServer, only: [call: 2]
+  import Enum, only: [each: 2]
 
-  def start(agent) do
-    Node.start(:"test@127.0.0.1")
-    Node.set_cookie(:elita)
-    {:ok, pid} = Elita.start_link(agent)
-    pid
+  def start agent do
+    Node.start :"test@127.0.0.1"
+    set_cookie :elita
+    start_link agent
   end
 
-  def verify(pid, q, expected_strings) do
-    response = Elita.act(q, pid)
-    Enum.each(expected_strings, &assert(String.contains?(response, &1)))
+  def verify(agent, q, a) when is_binary(a) do
+    response = call {:global, agent}, {:act, q}
+    assert String.contains? response, a
+  end
+
+  def verify(agent, q, a) when is_list(a) do
+    response = call {:global, agent}, {:act, q}
+    each a, &assert(String.contains? response, &1)
   end
 end
