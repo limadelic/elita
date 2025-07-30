4d2b2a360a43ab26662052271d8ee31e502e86b9
Maykel Suarez <maykel.suarez@ukg.com>
2025-07-30 11:45:33 -0400

    clean chat

--- lib/utils/chat.ex
+++ lib/utils/chat.ex
@@ -1,25 +1,29 @@
 defmodule Chat do
+  import String, only: [trim: 1, to_atom: 1]
+  import IO, only: [gets: 1, puts: 1]
+  import Elita, only: [start_link: 1, act: 2]
+  import Node, only: [start: 1]
 
   def main([name]) do
-    Node.start(:"#{name}@127.0.0.1")
-    {:ok, pid} = Elita.start_link(String.to_atom(name))
+    start(:"#{name}@127.0.0.1")
+    {:ok, pid} = start_link(to_atom(name))
     repl(name, pid)
   end
 
   defp repl(name, pid) do
-    IO.gets("#{name} > ")
+    gets("#{name} > ")
     |> repl(name, pid)
   end
 
   defp repl(:eof, _name, _pid) do
-    IO.puts("Bye!")
+    puts("Bye!")
   end
 
   defp repl(input, name, pid) when is_binary(input) do
     input
-    |> String.trim()
-    |> Elita.act(pid)
-    |> IO.puts()
+    |> trim()
+    |> act(pid)
+    |> puts()
     
     repl(name, pid)
   end
