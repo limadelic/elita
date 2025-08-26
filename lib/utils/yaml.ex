b38a5a70a448335ef91cd58eb5652bcda8d1a0c5
msuarz <msuarz@users.noreply.github.com>
2025-08-26 00:16:00 -0400

    moar yameling

--- lib/utils/yaml.ex
+++ lib/utils/yaml.ex
@@ -1,5 +1,5 @@
 defmodule Utils.Yaml do
-  import String, only: [replace_prefix: 3, to_atom: 1]
+  import String, only: [replace_prefix: 3, to_atom: 1, replace: 3]
   import Map, only: [new: 2]
   import Enum, only: [map: 2]
   import Ymlr, only: [document!: 1]
@@ -11,20 +11,25 @@ defmodule Utils.Yaml do
     |> document!()
     |> replace_prefix("---", "")
   rescue
-    _ -> "#{inspect args}"
+    _ -> "#{inspect(args)}"
   end
 
   def yaml(args) when is_binary(args) do
-    case decode(args) do
-      {:ok, parsed} when is_map(parsed) or is_list(parsed) ->
-        yaml(parsed)
-      _ -> args
-    end
-  rescue
-    _ -> args
+    yaml(
+      json(args) ||
+      json(replace(args, "'", "\""))
+    ) || args
   end
 
-  def yaml(args), do: "#{inspect args}"
+  def yaml(nil), do: nil
+  def yaml(args), do: "#{inspect(args)}"
+
+  defp json args do
+    case decode(args)do
+      {:ok, json} -> json
+      _ -> nil
+    end
+  end
 
   defp atomize(map) when is_map(map) do
     new(map, fn {k, v} -> {to_atom(k), atomize(v)} end)
