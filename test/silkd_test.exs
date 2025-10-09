612ef4db8a934d9b9b71e94a55d144b77ecb92a9
msuarz <msuarz@users.noreply.github.com>
2025-10-09 00:55:34 -0400

    focus

--- test/silkd_test.exs
+++ test/silkd_test.exs
@@ -11,28 +11,4 @@ defmodule SilkdTest do
     silkd("apply_for_a_job")
   end
 
-  test "close AIVA" do
-    Silkd.weave(:navigate, %{url: "https://rec-preview.dlas1.ucloud.int/MORDOR"})
-
-    content = Silkd.weave(:content)
-    assert content["content"] =~ "AIVA Chat"
-    assert content["content"] =~ ~s(style="display: block;")
-
-    result = Silkd.weave(:click, %{selector: "button[aria-label='Close AIVA Chat']", wait: 500})
-    assert result["status"] == "ok"
-
-    content = Silkd.weave(:content)
-    assert content["content"] =~ ~s(style="display: none;")
-  end
-
-  test "apply to engineering opportunity" do
-    Silkd.weave(:navigate, %{url: "https://rec-preview.dlas1.ucloud.int/MORDOR"})
-
-    search_input = ~s(input[aria-label="By job title, company, store or requisition number"])
-    Silkd.weave(:type, %{selector: search_input, text: "engineer"})
-    Silkd.weave(:press, %{key: "Enter", wait: 2000})
-
-    result = Silkd.weave(:click, %{selector: ~s(ukg-link[data-automation="job-title"])})
-    assert result["status"] == "ok"
-  end
 end
