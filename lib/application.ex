7a62e7fd160662365e67a837aa8fcb79d3391665
msuarz <mikemps@gmail.com>
2026-06-29 18:12:48 -0400

    Credo green lib/tools/user and application

--- lib/application.ex
+++ lib/application.ex
@@ -2,11 +2,14 @@ defmodule Elita.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      {Registry, keys: :unique, name: ElitaRegistry}
-    ]
+    Supervisor.start_link(children(), opts())
+  end
 
-    opts = [strategy: :one_for_one, name: Elita.Supervisor]
-    Supervisor.start_link(children, opts)
+  defp children do
+    [{Registry, keys: :unique, name: ElitaRegistry}]
+  end
+
+  defp opts do
+    [strategy: :one_for_one, name: Elita.Supervisor]
   end
 end
