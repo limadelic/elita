997cd6e9627f6460777fc5a0dc72f079400f8829
msuarz <mikemps@gmail.com>
2026-07-01 19:32:43 -0400

    swap default handler to raise-on-miss

--- config/test.exs
+++ config/test.exs
@@ -3,7 +3,7 @@ import Config
 tape_handler =
   case System.get_env("TAPE") do
     "rec" -> &Tape.Record.handle/3
-    _ -> &Tape.play/3
+    _ -> &Tape.Play.handle/3
   end
 
 config :elita, :tape_handler, tape_handler
