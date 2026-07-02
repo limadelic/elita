1ca532b981ecd6c969e301a36da1d1db363c7a61
msuarz <mikemps@gmail.com>
2026-07-01 23:36:07 -0400

    clock oracle from utc, not response

--- test/unit/clock_test.exs
+++ test/unit/clock_test.exs
@@ -18,22 +18,8 @@ defmodule ClockUnitTest do
   end
 
   test "clock responds with an hour" do
+    hour = Time.utc_now().hour
     result = ask :clock, "what hour is it?"
-
-    hour = extract_hour(result)
-    judge result, "states the current hour as #{hour} or within one hour of it"
-  end
-
-  defp extract_hour(text) do
-    case Regex.run(~r/(?:is|It is)\s+[\*]*(\d{1,2})[\*]*(?:\s+\(|$)/i, text) do
-      [_, hour] ->
-        h = String.to_integer(hour)
-        if h >= 0 and h <= 23, do: h, else: Time.utc_now().hour
-      _ ->
-        case Regex.run(~r/[\*]*(\d{1,2})[\*]*\s+\(\d{1,2}\s+(AM|PM)/, text) do
-          [_, hour, _] -> String.to_integer(hour)
-          _ -> Time.utc_now().hour
-        end
-    end
+    judge result, "states the current hour as #{hour} UTC, or within one hour of it, possibly expressed in another timezone"
   end
 end
