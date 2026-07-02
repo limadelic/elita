8963205fe66d675e02a46696d50a8183f33ea1e6
msuarz <mikemps@gmail.com>
2026-07-01 22:03:28 -0400

    drop sleep from ttt test

--- test/unit/ttt/ttt_test.exs
+++ test/unit/ttt/ttt_test.exs
@@ -4,11 +4,9 @@ defmodule TttUnitTest do
 
   setup do
     System.put_env("CASSETTE", "ttt")
-    System.put_env("MATCHER", "relaxed")
 
     on_exit(fn ->
       System.delete_env("CASSETTE")
-      System.delete_env("MATCHER")
     end)
 
     spawn(:alice, :ttt)
@@ -20,8 +18,6 @@ defmodule TttUnitTest do
   test "ttt agents play to finish" do
     tell(:bob, "alice is gonna be your opponent, wait for her move")
     tell(:alice, "start a game with bob, you are X, play first")
-
-    Process.sleep(15000)
 
     result = ask(:alice, "tell me: did the game finish and was it a win or tie?")
 
