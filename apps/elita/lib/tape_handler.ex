2a12963114daf68ffaa861f4cbdaf2fe16252321
msuarz <mikemps@gmail.com>
2026-07-04 19:54:16 -0400

    Make tape replay default, support TAPE=rec LIVE=1

--- apps/elita/lib/tape_handler.ex
+++ apps/elita/lib/tape_handler.ex
@@ -1,8 +1,9 @@
 defmodule TapeHandler do
   def handle(body, name, fun) do
-    handle_mode(body, name, fun, System.get_env("TAPE"))
+    handle_mode(body, name, fun, System.get_env("TAPE"), System.get_env("LIVE"))
   end
 
-  defp handle_mode(_body, _name, fun, nil), do: fun.()
-  defp handle_mode(body, name, fun, _), do: Tape.Play.handle(body, name, fun)
+  defp handle_mode(body, name, fun, "rec", _live), do: Tape.Record.handle(body, name, fun)
+  defp handle_mode(_body, _name, fun, _tape, "1"), do: fun.()
+  defp handle_mode(body, name, fun, _tape, _live), do: Tape.Play.handle(body, name, fun)
 end
