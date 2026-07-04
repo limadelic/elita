759cf221d059c6a59fc4d97aef2e9f5c4ab098ec
msuarz <mikemps@gmail.com>
2026-07-04 10:30:55 -0400

    Add root config and umbrella mix.exs

---
+++ mix.exs
@@ -0,0 +1,43 @@
+defmodule Elita.Umbrella do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elita_umbrella,
+      version: "0.1.0",
+      elixir: "~> 1.18",
+      apps_path: "apps",
+      config_path: "config/config.exs",
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp aliases do
+    [
+      test: [&run_test/1],
+      lint: [&run_lint/1]
+    ]
+  end
+
+  defp run_test(_) do
+    cmd = "cd apps/elita && mix test"
+    Mix.shell().cmd(cmd)
+  end
+
+  defp run_lint(_) do
+    cmd = "cd apps/elita && mix lint"
+    Mix.shell().cmd(cmd)
+  end
+
+  defp deps do
+    []
+  end
+end
