7a78dea2de80544942bc335ebc9e600afcbb437e
msuarz <mikemps@gmail.com>
2026-07-04 12:43:58 -0400

    Refactor Agent.Router to have smaller functions

--- apps/elita/lib/agent/router.ex
+++ apps/elita/lib/agent/router.ex
@@ -2,32 +2,47 @@ defmodule Agent.Router do
   import Elita, only: [call: 2, cast: 2]
 
   def route(name, :ask, message) do
-    case Agent.Registry.lookup(name) do
-      {:ok, {pid, _folder}} ->
-        Agent.Session.ask(pid, message)
-
-      {:error, :not_found} ->
-        try do
-          {:ok, call(name, message)}
-        rescue
-          _ -> {:error, :not_found}
-        end
-    end
+    Agent.Registry.lookup(name)
+    |> ask_route(name, message)
   end
 
   def route(name, :tell, message) do
-    case Agent.Registry.lookup(name) do
-      {:ok, {pid, _folder}} ->
-        Agent.Session.cast(pid, message)
-        :ok
+    Agent.Registry.lookup(name)
+    |> tell_route(name, message)
+  end
 
-      {:error, :not_found} ->
-        try do
-          cast(name, message)
-          :ok
-        rescue
-          _ -> {:error, :not_found}
-        end
-    end
+  defp ask_route({:ok, {pid, _}}, _name, message) do
+    Agent.Session.ask(pid, message)
+  end
+
+  defp ask_route({:error, _}, name, message) do
+    markdown_ask(name, message)
+  end
+
+  defp tell_route({:ok, {pid, _}}, _name, message) do
+    Agent.Session.cast(pid, message)
+    :ok
+  end
+
+  defp tell_route({:error, _}, name, message) do
+    markdown_tell(name, message)
+  end
+
+  defp markdown_ask(name, message) do
+    call(name, message)
+    |> wrap_ok()
+  rescue
+    _ -> {:error, :not_found}
+  end
+
+  defp markdown_tell(name, message) do
+    cast(name, message)
+    :ok
+  rescue
+    _ -> {:error, :not_found}
+  end
+
+  defp wrap_ok(value) do
+    {:ok, value}
   end
 end
