8708ccddcca00608837a6ce686d3e20f2926be8c
msuarz <mikemps@gmail.com>
2026-07-04 12:40:22 -0400

    Make tape handler more resilient to missing Tape.Writer process

--- apps/el/test/test_helper.exs
+++ apps/el/test/test_helper.exs
@@ -2,5 +2,11 @@ ExUnit.start()
 
 # Start Tape.Writer if TAPE is set (for cassette replay in integration tests)
 if System.get_env("TAPE") do
-  {:ok, _} = Tape.Writer.start_link(nil)
+  try do
+    {:ok, _} = Tape.Writer.start_link(nil)
+  rescue
+    UndefinedFunctionError ->
+      # Tape.Writer might not be compiled in this context
+      :ok
+  end
 end
--- apps/elita/lib/tape_handler.ex
+++ apps/elita/lib/tape_handler.ex
@@ -1,8 +1,22 @@
 defmodule TapeHandler do
   def handle(body, name, fun) do
     case System.get_env("TAPE") do
-      nil -> fun.()
-      _ -> Tape.Play.handle(body, name, fun)
+      nil ->
+        fun.()
+
+      _ ->
+        try do
+          # Try to use tape if Tape.Writer is available
+          Tape.Play.handle(body, name, fun)
+        rescue
+          e ->
+            # If tape system fails (e.g., Tape.Writer not started), fall through to real call
+            if String.contains?(Exception.message(e), "no process") do
+              fun.()
+            else
+              reraise(e, __STACKTRACE__)
+            end
+        end
     end
   end
 end
