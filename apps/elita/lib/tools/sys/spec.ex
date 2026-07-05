bfbf6c16a4106ba652be2e0a54aba23012d5040c
msuarz <mikemps@gmail.com>
2026-07-04 23:50:42 -0400

    Restore spec.ex: string interpolation

--- apps/elita/lib/tools/sys/spec.ex
+++ apps/elita/lib/tools/sys/spec.ex
@@ -19,9 +19,9 @@ defmodule Tools.Sys.Spec do
   defdelegate spec(name, state), to: Tools.Sys.Spec.Schema, as: :get
 
   def exec(_, %{"name" => name}, %{config: config} = state) do
-    name = name <> "_spec"
+    name = "#{name}_spec"
     spec = Cfg.config(name)
-    log("🧪", name, ":", "\n" <> spec.content <> "\n", :white)
+    log("🧪", name, ":", "\n#{spec.content}\n", :white)
     {spec.content, %{state | config: config ++ [spec]}}
   end
 end
