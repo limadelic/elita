ae096df0c7b1497d1f288a210ef0439b8e273af7
msuarz <mikemps@gmail.com>
2026-07-04 23:16:09 -0400

    Import Kernel raise and inspect

--- apps/elita/test/tape/play.ex
+++ apps/elita/test/tape/play.ex
@@ -2,6 +2,7 @@ defmodule Tape.Play do
   import Tape.Matcher, only: [contains: 2]
   import Tape.Writer, only: [claim_agent: 4, claim: 3]
   import Tape.Store, only: [load_entries: 0]
+  import Kernel, only: [raise: 2, inspect: 1]
 
   def handle(body, name, fun) do
     ensure_entries(load())
