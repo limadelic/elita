cf3cb74da33fc9d4b94f09fa1eafc39032b5af54
msuarz <mikemps@gmail.com>
2026-07-04 22:53:49 -0400

    Import System.get_env with 2 arity

--- apps/elita/lib/llm/llm.ex
+++ apps/elita/lib/llm/llm.ex
@@ -1,9 +1,11 @@
 defmodule Llm do
+  import System, only: [get_env: 2]
+
   def llm(text) when is_binary(text), do: llm_impl(text)
   def llm(state), do: llm_impl(state)
 
   defp llm_impl(arg) do
-    System.get_env("LLM", "lite")
+    get_env("LLM", "lite")
     |> select_backend
     |> apply(:llm, [arg])
   end
