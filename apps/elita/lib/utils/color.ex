3d916b84730581865d50231daeb1a71cf51f1a27
msuarz <mikemps@gmail.com>
2026-07-04 23:17:55 -0400

    Fix Imports: color.ex remove qualifiedcalls

--- apps/elita/lib/utils/color.ex
+++ apps/elita/lib/utils/color.ex
@@ -1,7 +1,8 @@
 defmodule Color do
+  import Kernel
   import IO, only: [puts: 1]
 
   def puts(text, code) do
-    puts("\e[38;5;#{code}m#{text}\e[0m")
+    puts("\e[38;5;" <> to_string(code) <> "m" <> text <> "\e[0m")
   end
 end
