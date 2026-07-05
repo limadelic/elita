b42f2a0762938ad887756b3ad9a76fa5f5b84dde
msuarz <mikemps@gmail.com>
2026-07-05 17:12:04 -0400

    Fix restore: correct native tell and ask

--- apps/el/lib/el/commands/ask.ex
+++ apps/el/lib/el/commands/ask.ex
@@ -1,15 +1,9 @@
 defmodule El.Commands.Ask do
-  import Elita, only: [start_link: 2]
-  import String, only: [to_atom: 1]
-  import IO, only: [puts: 1]
-  import Agent.Router, only: [route: 3]
+  import Elita, only: [start_link: 2, call: 2]
 
   def execute(agent, msg) do
     {:ok, _pid} = start_link(agent, [agent])
-    route(to_atom(agent), :ask, msg) |> output |> puts()
+    result = call(agent, msg)
+    IO.puts(result)
   end
-
-  defp output({:ok, resp}), do: resp
-  defp output({:error, :not_found}), do: "not found"
-  defp output(resp), do: resp
 end
--- apps/el/lib/el/commands/tell.ex
+++ apps/el/lib/el/commands/tell.ex
@@ -1,10 +1,8 @@
 defmodule El.Commands.Tell do
-  import Elita, only: [start_link: 2]
-  import String, only: [to_atom: 1]
-  import Agent.Router, only: [route: 3]
+  import Elita, only: [start_link: 2, cast: 2]
 
   def execute(agent, msg) do
     {:ok, _pid} = start_link(agent, [agent])
-    route(to_atom(agent), :tell, msg)
+    cast(agent, msg)
   end
 end
