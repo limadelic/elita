741b1e6b180a6a4bb54c60cc6f5a45a12e2525e8
msuarz <mikemps@gmail.com>
2026-07-04 23:12:31 -0400

    Fix import violations in tools/index.ex

--- apps/elita/lib/tools/index.ex
+++ apps/elita/lib/tools/index.ex
@@ -1,15 +1,16 @@
 defmodule Tool.Index do
+  import Kernel
   import Tools, only: [exec: 2]
 
   def set(key, value) do
     {result, _state} =
-      exec(%{"name" => "set", "args" => %{"key" => "#{key}", "value" => value}}, %{})
+      exec(%{"name" => "set", "args" => %{"key" => "" <> key, "value" => value}}, %{})
 
     result
   end
 
   def get(key) do
-    {result, _state} = exec(%{"name" => "get", "args" => %{"key" => "#{key}"}}, %{})
+    {result, _state} = exec(%{"name" => "get", "args" => %{"key" => "" <> key}}, %{})
     result
   end
 
@@ -19,6 +20,6 @@ defmodule Tool.Index do
   end
 
   defp tell_spec(target, message) do
-    %{"name" => "tell", "args" => %{"recipient" => "#{target}", "message" => "#{message}"}}
+    %{"name" => "tell", "args" => %{"recipient" => "" <> target, "message" => "" <> message}}
   end
 end
