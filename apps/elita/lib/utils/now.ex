8a2a4c388994fe0aa1dd84e5c93dc3ddb23b6585
msuarz <mikemps@gmail.com>
2026-07-05 00:00:36 -0400

    Import calendar, remove module prefix

--- apps/elita/lib/utils/now.ex
+++ apps/elita/lib/utils/now.ex
@@ -1,11 +1,12 @@
 defmodule Now do
   import Application, only: [get_env: 3]
+  import :calendar, only: [local_time: 0]
 
   def time do
     get_env(:elita, :clock, &default/0).()
   end
 
   defp default do
-    :calendar.local_time()
+    local_time()
   end
 end
