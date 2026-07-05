2ea75875b011a77f27528f7ce3b6c333cbf9f689
msuarz <mikemps@gmail.com>
2026-07-04 23:13:59 -0400

    Fix Imports: now.ex replace Erlang call

--- apps/elita/lib/utils/now.ex
+++ apps/elita/lib/utils/now.ex
@@ -1,11 +1,12 @@
 defmodule Now do
   import Application, only: [get_env: 3]
+  import Kernel
 
   def time do
     get_env(:elita, :clock, &default/0).()
   end
 
   defp default do
-    :calendar.local_time()
+    DateTime.now!(:local) |> DateTime.to_time()
   end
 end
