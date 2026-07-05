1f6e26e45fe9cf55649fa66e4cdcffd6bcbf1f61
msuarz <mikemps@gmail.com>
2026-07-04 20:43:05 -0400

    Refactor Application.start for simplicity and lint

--- apps/elita/lib/application.ex
+++ apps/elita/lib/application.ex
@@ -4,17 +4,17 @@ defmodule Elita.Application do
 
   def start(_type, _args) do
     init_global()
-    {:ok, _} = Supervisor.start_link(children(), opts())
+    start_supervisor()
     Agent.Registry.create()
     Agent.Manager.start_agents()
     {:ok, self()}
   end
 
-  defp children do
-    [{Registry, keys: :unique, name: ElitaRegistry}]
-  end
-
-  defp opts do
-    [strategy: :one_for_one, name: Elita.Supervisor]
+  defp start_supervisor do
+    Supervisor.start_link(
+      [Registry.child_spec(keys: :unique, name: ElitaRegistry)],
+      strategy: :one_for_one,
+      name: Elita.Supervisor
+    )
   end
 end
