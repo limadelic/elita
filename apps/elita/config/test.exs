e82682c3cbcf2a866b8d3b3a5f72b12bfafddc40
msuarz <mikemps@gmail.com>
2026-07-05 17:48:46 -0400

    Pin test clock for replay

--- apps/elita/config/test.exs
+++ apps/elita/config/test.exs
@@ -6,3 +6,16 @@ tape_handler =
     {_, "1"} -> fn _body, _agent_name, fun -> fun.() end
     {_, _} -> &Tape.Play.handle/3
   end
+
+clock_fn =
+  case {System.get_env("TAPE"), System.get_env("LIVE")} do
+    {"rec", _} -> nil
+    {_, "1"} -> nil
+    {_, _} -> fn -> {{2025, 7, 7}, {10, 0, 0}} end
+  end
+
+config :elita, :tape_handler, tape_handler
+
+if clock_fn do
+  config :elita, :clock, clock_fn
+end
