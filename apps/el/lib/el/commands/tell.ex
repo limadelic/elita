ef5cbd5ef0330aa279c9377055d35c80c9535770
msuarz <mikemps@gmail.com>
2026-07-06 15:12:18 -0400

    Add EL_NODE support to tell.ex

--- apps/el/lib/el/commands/tell.ex
+++ apps/el/lib/el/commands/tell.ex
@@ -3,16 +3,39 @@ defmodule El.Commands.Tell do
   import Elita, only: [start_link: 2, cast: 2]
   alias El.Distribution
 
-  def execute(agent, msg) do
+  def execute(agent, msg, opts \\ []) do
     Distribution.start()
-    claude_target = :"claude_#{agent}@127.0.0.1"
+    env_module = Keyword.get(opts, :env_module, El.Infra.Env)
+
+    case remote_target(agent, env_module: env_module) do
+      nil -> default(agent, msg)
+      target -> attempt_inject(msg, target, agent, env_module)
+    end
+  end
+
+  def remote_target(agent, opts \\ []) do
+    env_module = Keyword.get(opts, :env_module, El.Infra.Env)
+
+    case env_module.get("EL_NODE") do
+      nil -> nil
+      host -> :"claude_#{agent}@#{host}"
+    end
+  end
+
+  defp attempt_inject(msg, target, agent, env_module) do
     process_name = String.to_atom(agent)
 
-    if Node.connect(claude_target) do
-      inject(msg, claude_target, process_name)
+    if Node.connect(target) do
+      inject(msg, target, process_name)
     else
+      host = env_module.get("EL_NODE")
+      remote_unreachable(agent, host)
       default(agent, msg)
     end
+  end
+
+  def remote_unreachable(agent, host) do
+    IO.write(:stderr, "session #{agent} unreachable at #{host}\n")
   end
 
   defp inject(msg, target, process_name) do
