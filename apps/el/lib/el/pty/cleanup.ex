6a3194ea13d4e31ffc2f86b717aaf47e417713e5
msuarz <mikemps@gmail.com>
2026-07-06 16:44:01 -0400

    Extract kill_group sequence to named function

--- apps/el/lib/el/pty/cleanup.ex
+++ apps/el/lib/el/pty/cleanup.ex
@@ -3,18 +3,19 @@ defmodule El.Pty.Cleanup do
 
   def kill_group(nil), do: :ok
   def kill_group(pid) do
-    # Kill the process group that the spawned process belongs to
-    # Script is the group leader, so killing negative pid kills the group
-    signal(pid, "-TERM")
-    Process.sleep(100)
-    signal(pid, "-9")
-    :ok
+    kill_sequence(pid)
   rescue
     _ -> :ok
   end
 
+  defp kill_sequence(pid) do
+    signal(pid, "-TERM")
+    Process.sleep(100)
+    signal(pid, "-9")
+    :ok
+  end
+
   defp signal(pid, sig) do
-    # Kill negative pid to kill the entire process group
     System.cmd("kill", [sig, "-#{pid}"])
   rescue
     _ -> :ok
