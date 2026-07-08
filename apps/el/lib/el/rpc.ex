14230644854c87fffbfbaa034bf053baa245d5f6
msuarz <mikemps@gmail.com>
2026-07-07 20:11:29 -0400

    fix: add imports to rpc.ex, resolve credo violations

--- apps/el/lib/el/rpc.ex
+++ apps/el/lib/el/rpc.ex
@@ -2,11 +2,11 @@ defmodule El.RPC do
   @moduledoc false
 
   import Application, only: [ensure_all_started: 1]
+  import File, only: [cwd!: 0]
+  import El.Commands.Ask, only: [execute: 2]
+  import El.Commands.Ls, only: [remote: 1]
 
-  alias El.Commands.Ask
-  alias El.Commands.Ls
-
-  def dispatch(command, cwd \\ File.cwd!()) do
+  def dispatch(command, cwd \\ cwd!()) do
     ensure_all_started(:elita)
     build(safe(command, cwd))
   end
@@ -17,8 +17,8 @@ defmodule El.RPC do
 
   defp safe(command, cwd), do: handle(command, cwd)
 
-  defp handle(["ls"], cwd), do: Ls.remote(cwd: cwd)
-  defp handle(["ask", agent, msg], _cwd), do: Ask.execute(agent, msg)
+  defp handle(["ls"], cwd), do: remote(cwd: cwd)
+  defp handle(["ask", agent, msg], _cwd), do: execute(agent, msg)
   defp handle(_, _cwd), do: ""
 
   defp marker, do: "node: #{Node.self()}"
