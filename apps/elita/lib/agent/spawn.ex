3d21fcff5b161f7ee2e820d50fc5dd384f47a598
msuarz <mikemps@gmail.com>
2026-07-07 21:14:15 -0400

    add cmd to System import in spawn

--- apps/elita/lib/agent/spawn.ex
+++ apps/elita/lib/agent/spawn.ex
@@ -2,7 +2,7 @@ defmodule Agent.Spawn do
   import Logger, only: [error: 1, warning: 1]
   import Port, only: [open: 2, close: 1]
   import String, only: [trim: 1]
-  import System, only: [find_executable: 1]
+  import System, only: [cmd: 2, find_executable: 1]
 
   def run(message, folder) do
     cmd = {:spawn_executable, exe()}
@@ -51,7 +51,7 @@ defmodule Agent.Spawn do
 
   defp slay(port) do
     {:os_pid, pid} = :erlang.port_info(port, :os_pid)
-    System.cmd("kill", [pid |> to_string()])
+    cmd("kill", [pid |> to_string()])
   rescue
     _ -> :ok
   end
