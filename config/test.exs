de42ce7f67e6349a01b2384870b692d1cb863168
msuarz <mikemps@gmail.com>
2026-07-08 11:22:39 -0400

    Replace anonymous clock function with capture reference.

--- config/test.exs
+++ config/test.exs
@@ -3,4 +3,4 @@ import Config
 # Configure the tape handler - it checks TAPE at runtime
 config :elita, tape_handler: &TapeHandler.handle/3
 
-config :elita, :clock, fn -> {{2025, 7, 7}, {10, 0, 0}} end
+config :elita, :clock, &Clock.test_time/0
