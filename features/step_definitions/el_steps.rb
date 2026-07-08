7b3d7837174944c02bf619d1dcac94949d142204
msuarz <mikemps@gmail.com>
2026-07-08 00:44:55 -0400

    Add retry wrapper for async table assertions

--- features/step_definitions/el_steps.rb
+++ features/step_definitions/el_steps.rb
@@ -9,8 +9,20 @@ When(/^> el (tell .+)$/) do |args|
 end
 
 When(/^(\w+)> ([^:]+):$/) do |prompt, input, table|
-  output = send(input, prompt)
-  verify_table(table, output)
+  max_attempts = 5
+  max_attempts.times do |attempt|
+    begin
+      output = send(input, prompt)
+      verify_table(table, output)
+      break  # Success
+    rescue => e
+      if attempt < max_attempts - 1
+        sleep 0.1 if ENV["TAPE"] == "rec"  # Backoff only during live recording
+      else
+        raise e  # Final attempt failed
+      end
+    end
+  end
 end
 
 When(/^(\w+)> ([^:]+)$/) do |prompt, input|
