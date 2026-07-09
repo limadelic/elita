9b53762c5d894e35165dfe3d1bd1f44e3d7736fa
msuarz <mikemps@gmail.com>
2026-07-09 02:56:32 -0400

    Guard clause in wait_for_prompt loop

--- features/support/repl_helper.rb
+++ features/support/repl_helper.rb
@@ -111,15 +111,14 @@ module ReplHelper
     begin
       while Time.now < timeout
         ready = IO.select([@reader], nil, nil, 0.1)
-        if ready
-          chunk = @reader.readpartial(4096)
-          chunk = safe_encode(chunk)
-          output << chunk
-          @transcript << chunk if @transcript
-          stripped_chunk = safe_encode(strip_ansi(chunk))
-          @transcript_stripped << stripped_chunk if @transcript_stripped
-          return output if output.include?(pattern)
-        end
+        next unless ready
+        chunk = @reader.readpartial(4096)
+        chunk = safe_encode(chunk)
+        output << chunk
+        @transcript << chunk if @transcript
+        stripped_chunk = safe_encode(strip_ansi(chunk))
+        @transcript_stripped << stripped_chunk if @transcript_stripped
+        return output if output.include?(pattern)
       end
     rescue EOFError
     end
