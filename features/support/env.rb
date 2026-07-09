f13c65e8358b30a029c6c3de2b679c528eb5fc8f
msuarz <mikemps@gmail.com>
2026-07-09 00:26:29 -0400

    Revert "Extract build logic into named method"
    This reverts commit 1d012e4ca92ddec5dd45b83749b7cdfb95229e2a.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,5 @@
 require "rspec/expectations"
 
 BeforeAll do
-  build!
-end
-
-private
-
-def build!
-  system("cd apps/el && mix escript.build 2>&1 > /dev/null") or
-    abort("mix escript.build failed")
+  system("cd apps/el && mix escript.build 2>&1 > /dev/null") or abort("mix escript.build failed")
 end
