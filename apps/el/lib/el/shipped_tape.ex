e15b35810cb35a9a6b19d19d55a1e4e4246d32de
msuarz <mikemps@gmail.com>
2026-07-08 22:41:17 -0400

    spec: shipped tape decision cache

---
+++ apps/el/lib/el/shipped_tape.ex
@@ -0,0 +1,20 @@
+defmodule El.ShippedTape do
+  import System, only: [get_env: 1, put_env: 2]
+  import Path, only: [join: 2, dirname: 1]
+
+  def setup, do: maybe_set_defaults(get_env("MIX_ENV"), get_env("CASSETTE"))
+
+  defp maybe_set_defaults("test", _), do: :ok
+  defp maybe_set_defaults(_, cassette) when cassette != nil, do: :ok
+  defp maybe_set_defaults(_, _), do: set_defaults()
+
+  defp set_defaults do
+    put_env("CASSETTE", "el")
+    put_env("CASSETTE_DIR", shipped_dir())
+    put_env("TAPE_ON_MISS", "live")
+  end
+
+  defp shipped_dir do
+    __DIR__ |> dirname() |> dirname() |> join("cassettes")
+  end
+end
