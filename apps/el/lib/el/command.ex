babd57ec4fd99bfef20d62b506fa0659d2988dc4
msuarz <mikemps@gmail.com>
2026-07-09 01:09:11 -0400

    command imports

--- apps/el/lib/el/command.ex
+++ apps/el/lib/el/command.ex
@@ -1,14 +1,11 @@
 defmodule El.Command do
   @moduledoc false
 
-  alias El.Command.Ls
-  alias El.Command.Delegator
-
-  defdelegate ls(path), to: Ls, as: :run
-  defdelegate ask(agent, msg, tool \\ nil), to: Delegator
-  defdelegate tell(agent, msg, tool \\ nil), to: Delegator
-  defdelegate spawn(name, agent), to: Delegator
-  defdelegate claude(name), to: Delegator
-  defdelegate cd(path), to: Delegator
-  defdelegate daemon(), to: Delegator
+  defdelegate ls(path), to: El.Command.Ls, as: :run
+  defdelegate ask(agent, msg, tool \\ nil), to: El.Command.Delegator
+  defdelegate tell(agent, msg, tool \\ nil), to: El.Command.Delegator
+  defdelegate spawn(name, agent), to: El.Command.Delegator
+  defdelegate claude(name), to: El.Command.Delegator
+  defdelegate cd(path), to: El.Command.Delegator
+  defdelegate daemon(), to: El.Command.Delegator
 end
