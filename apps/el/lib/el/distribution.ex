72c57265a93a72b2876e3857cd3983d9b9d41904
msuarz <mikemps@gmail.com>
2026-07-10 06:14:44 -0400

    Fix registry import and reduce module complexity

--- apps/el/lib/el/distribution.ex
+++ apps/el/lib/el/distribution.ex
@@ -11,6 +11,7 @@ defmodule El.Distribution do
   import Keyword, only: [get: 3]
   import File, only: [cwd!: 0]
   import Path, only: [basename: 1]
+  import Registry, only: [lookup: 2]
 
   def start(name \\ :default, opts \\ []) do
     node_name = node(name, opts)
@@ -18,46 +19,36 @@ defmodule El.Distribution do
   end
 
   def target(name) do
-    case {name, :puppet} |> :global.whereis_name() do
-      :undefined -> lookup_registry(name)
-      pid -> pid
-    end
+    :global.whereis_name({name, :puppet}) |> pick(name)
   rescue
-    _ -> lookup_registry(name)
+    _ -> find(name)
   end
 
-  defp lookup_registry(name) do
-    case Registry.lookup(ElitaRegistry, name) do
-      [{pid, %{kind: :puppet}}] -> pid
-      _ -> nil
-    end
+  defp pick(:undefined, name), do: find(name)
+  defp pick(pid, _), do: pid
+
+  defp find(name) do
+    lookup(ElitaRegistry, name) |> extract()
   rescue
     _ -> nil
   end
 
+  defp extract([{pid, %{kind: :puppet}}]), do: pid
+  defp extract(_), do: nil
   def daemon do
     boot(:"elita@127.0.0.1", :longnames)
     ensure_all_started(:elita)
-    redial()
+    (try do load() |> each(&connect/1) rescue _ -> :ok end)
     sleep(:infinity)
   end
-
-  defp redial do
-    load() |> each(&connect/1)
-  rescue
-    _ -> :ok
-  end
-
   defp node(name, opts) do
     :"claude_#{session(name)}@#{get(opts, :host, host())}"
   end
 
   defp boot(node_name, mode) do
-    retry(fn -> Node.start(node_name, mode) end, 5) |> act(node_name, mode)
-  end
-
-  defp retry(fun, tries) do
-    attempt(fun.(), fun, tries)
+    fn -> Node.start(node_name, mode) end
+    |> then(&attempt(&1.(), &1, 5))
+    |> act(node_name, mode)
   end
 
   defp attempt({:ok, pid}, _fun, _tries) do
@@ -70,7 +61,7 @@ defmodule El.Distribution do
 
   defp attempt({:error, _reason}, fun, tries) when tries > 1 do
     sleep(200)
-    retry(fun, tries - 1)
+    attempt(fun.(), fun, tries - 1)
   end
 
   defp attempt({:error, _reason}, _fun, _tries) do
@@ -92,27 +83,14 @@ defmodule El.Distribution do
     :ok
   end
 
-  defp mode(h) do
-    format(contains?(h, "."))
-  end
-
-  defp format(true), do: :longnames
-  defp format(false), do: :shortnames
-
   def naming(opts) do
-    host_value = get(opts, :host, "127.0.0.1")
-    mode(host_value)
+    if contains?(get(opts, :host, "127.0.0.1"), "."), do: :longnames, else: :shortnames
   end
 
   def fetch(opts \\ []) do
     get(opts, :host, host())
   end
 
-  defp session(:default) do
-    cwd!() |> basename()
-  end
-
-  defp session(name) when is_binary(name) do
-    name
-  end
+  defp session(:default), do: cwd!() |> basename()
+  defp session(name), do: name
 end
