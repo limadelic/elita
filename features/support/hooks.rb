d906870cf080474ee19a4a3c8bfe6f0b07c065a3
msuarz <mikemps@gmail.com>
2026-07-09 23:03:12 -0400

    Fix detached script processes in After hook

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -29,12 +29,44 @@ end
 After do
   if @pid
     begin
-      Process.kill("TERM", @pid)
+      pgid = Process.getpgid(@pid)
+      Process.kill("TERM", -pgid)
+      sleep 0.1
+      Process.kill("KILL", -pgid)
+    rescue Errno::ESRCH
+    rescue Errno::EPERM
+    end
+
+    begin
       Process.wait(@pid, Process::WNOHANG)
     rescue Errno::ESRCH
     end
+
+    # Kill any orphaned script processes from this cassette
+    kill_orphaned_scripts
   end
   @reader.close if @reader && !@reader.closed?
   @writer.close if @writer && !@writer.closed?
   FileUtils.rm_rf(@scratch) if @scratch && File.exist?(@scratch)
+end
+
+def kill_orphaned_scripts
+  orphans = find_script_orphans
+  terminate_pids(orphans, "TERM")
+  sleep 0.1
+  terminate_pids(orphans, "KILL")
+end
+
+def find_script_orphans
+  cmd = "ps aux | grep 'script -q /dev/null' | grep -v grep | awk '{print $2}'"
+  `#{cmd}`.strip.split("\n")
+end
+
+def terminate_pids(pids, signal)
+  pids.each do |pid_str|
+    pid = pid_str.to_i
+    next if pid.zero?
+
+    Process.kill(signal, pid) rescue Errno::ESRCH
+  end
 end
