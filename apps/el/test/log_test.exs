d51c0db29ba12ea579a219ee2c1764a5f1c33422
msuarz <mikemps@gmail.com>
2026-07-10 15:20:17 -0400

    Drop unit tests, replay suite is the gate

--- apps/el/test/log_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule El.LogTest do
-  use ExUnit.Case
-
-  test "setup creates log directory and returns path" do
-    path = El.Log.setup("test", [])
-    assert String.match?(path, ~r/\.elita.*sessions.*test_\d+\.log/)
-  end
-end
