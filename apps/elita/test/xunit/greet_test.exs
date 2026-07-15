07ae00add5f99fb0c628eaecd32195b4845e41f4
msuarz <mikemps@gmail.com>
2026-07-15 16:14:24 -0400

    greet: align cassette judge expectations

--- apps/elita/test/xunit/greet_test.exs
+++ apps/elita/test/xunit/greet_test.exs
@@ -6,8 +6,8 @@ defmodule GreetTest do
     spawn(:greet)
     spawn(:judge)
 
-    judge ask(:greet, "hello"), "the greeter asks a question to identify who they are talking to"
-    judge ask(:greet, "Mike"), "the greeter acknowledges and uses the name Mike"
-    judge ask(:greet, "how are you?"), "the greeter identifies itself as Greeeet"
+    judge ask(:greet, "hello"), "asks who they are talking to"
+    judge ask(:greet, "Mike"), "greets Mike by name"
+    judge ask(:greet, "how are you?"), "identifies as Greeeet"
   end
 end
