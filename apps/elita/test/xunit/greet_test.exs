9cd041b5c7f8a5e3441916c6fc63a23fa411abab
msuarz <mikemps@gmail.com>
2026-07-15 16:13:18 -0400

    greet: judge asserts behavior

--- apps/elita/test/xunit/greet_test.exs
+++ apps/elita/test/xunit/greet_test.exs
@@ -4,9 +4,10 @@ defmodule GreetTest do
 
   test "greet conversation flow" do
     spawn(:greet)
+    spawn(:judge)
 
-    verify(:greet, "Who am I talking to", "hello")
-    verify(:greet, "Mike", "Mike")
-    verify(:greet, "I am Greeeet", "how are you?")
+    judge ask(:greet, "hello"), "the greeter asks a question to identify who they are talking to"
+    judge ask(:greet, "Mike"), "the greeter acknowledges and uses the name Mike"
+    judge ask(:greet, "how are you?"), "the greeter identifies itself as Greeeet"
   end
 end
