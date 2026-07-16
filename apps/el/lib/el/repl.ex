93bf1d122eab2da8f4a209450d3c2c2d44f90376
msuarz <mikemps@gmail.com>
2026-07-16 11:33:48 -0400

    birth: el log verb, agent attribution

--- apps/el/lib/el/repl.ex
+++ apps/el/lib/el/repl.ex
@@ -7,93 +7,88 @@ defmodule El.REPL do
   import Agent.Harness, only: [dispatch: 3]
   import El.Log.Reply, only: [handle: 2]
   import IO, only: [read: 2, puts: 1, write: 1]
-  import String, only: [trim: 1, split: 3]
-
+  import String, only: [trim: 1, split: 3, starts_with?: 2]
+  import File, only: [ls: 1, read!: 1]
+  import Path, only: [expand: 1, join: 2]
+  import Enum, only: [filter: 2, sort: 1]
+  import List, only: [last: 1]
   def run(agent) do
     ensure_all_started(:elita)
-    puppet = init(agent)
-    loop(agent, puppet)
+    loop(agent, init(agent))
   end
-
   defp init(agent) do
-    setup()
+    reg() |> settle()
+    tape() |> settle()
     boot(agent)
     find(agent)
   end
-
-  defp setup do
+  defp reg do
     import Registry, only: [start_link: 1]
-    start_link(keys: :unique, name: ElitaRegistry) |> settle()
-    tape() |> settle()
+    start_link(keys: :unique, name: ElitaRegistry)
   end
-
+  defp tape do
+    import Tape.Writer, only: [start_link: 1]
+    start_link(nil)
+  end
   defp find(agent) do
     agent |> reach() |> pick(agent)
   rescue
     _ -> native(agent)
   end
-
   defp pick(nil, agent), do: native(agent)
   defp pick(:undefined, agent), do: native(agent)
   defp pick(pid, _agent), do: pid
-
   defp native(agent) do
     import Elita, only: [spawn: 2]
     spawn(agent, [agent]) |> settle()
     nil
   end
-
-  defp tape do
-    import Tape.Writer, only: [start_link: 1]
-    start_link(nil)
-  end
-
   defp settle({:ok, _pid}), do: :ok
   defp settle({:error, {:already_started, _pid}}), do: :ok
   defp settle({:error, _}), do: :ok
-
   defp loop(agent, puppet) do
-    prompt(agent)
-    read(:stdio, :line) |> process(agent, puppet)
+    write("#{agent}> ")
+    read(:stdio, :line) |> next(agent, puppet)
   end
-
-  defp prompt(agent), do: write("#{agent}> ")
-
-  defp process(:eof, _agent, _puppet), do: :ok
-
-  defp process(line, agent, puppet) do
+  defp next(:eof, _agent, _puppet), do: :ok
+  defp next(line, agent, puppet) do
     handle(agent, puppet, trim(line)) |> proceed(agent, puppet)
   end
-
   defp proceed(:stop, _agent, _puppet), do: :ok
   defp proceed(:ok, agent, puppet), do: loop(agent, puppet)
-
   defp handle(_agent, _puppet, ""), do: :ok
-
   defp handle(_agent, _puppet, "/exit"), do: :stop
-
   defp handle(agent, puppet, input) when is_pid(puppet) do
-    route(agent, puppet, input) |> reply(agent)
+    input |> split(" ", parts: 2) |> route(agent, puppet, input)
   end
-
-  defp handle(agent, nil, input) do
-    dispatch(agent, input, :ask) |> puts()
+  defp handle(agent, nil, "log"), do: agent |> log() |> puts()
+  defp handle(agent, nil, input), do: dispatch(agent, input, :ask) |> puts()
+  defp handle(agent, :undefined, "log"), do: agent |> log() |> puts()
+  defp handle(agent, :undefined, input), do: dispatch(agent, input, :ask) |> puts()
+  defp route([name, "log"], _a, _p, _i), do: name |> log() |> puts()
+  defp route(_x, _agent, puppet, input) do
+    {response, target} = input |> split(" ", parts: 2) |> via(puppet, input)
+    handle(response, target)
   end
-
-  defp handle(agent, :undefined, input) do
-    dispatch(agent, input, :ask) |> puts()
-  end
-
-  defp route(_a, p, i), do: i |> split(" ", parts: 2) |> via(p, i)
-
-  defp via([_w], p, i), do: ask(p, i)
-  defp via([w, _], p, i), do: ask(choose(wait(w), p), i)
-
+  defp via([_w], p, i), do: {ask(p, i), from(p)}
+  defp via([w, _], p, i), do: {ask(choose(wait(w), p), i), w}
+  defp via(_, p, i), do: {ask(p, i), from(p)}
+  defp from(pid) when is_pid(pid), do: "el"
+  defp from(_), do: "el"
   defp choose(nil, default), do: default
   defp choose(t, _), do: t
-
-  defp reply(response, agent) do
-    handle(response, agent)
-    :ok
+  defp log(""), do: ""
+  defp log(agent) do
+    expand("~/.elita/sessions") |> ls() |> open(agent)
+  rescue
+    _ -> ""
+  end
+  defp open({:ok, f}, agent) do
+    f |> filter(&starts_with?(&1, "#{agent}_")) |> sort() |> last() |> load(agent)
+  end
+  defp open({:error, _}, _agent), do: ""
+  defp load(nil, _agent), do: ""
+  defp load(file, _agent) do
+    expand("~/.elita/sessions") |> join(file) |> read!()
   end
 end
--- features/elita/birth.feature
+++ features/elita/birth.feature
@@ -4,15 +4,9 @@ Feature: Birth
     * > el
 
     * el> mother it's time to give birth
-      | ✨ el | beautiful baby  |
-      | ✨ el | contractions    |
-      | ✨ el | pain is intense |
-      | ✨ el | it's happening  |
-      | ✨ el | bring my baby   |
+      | ✨ mother | gasping |
+      | ✨ mother | baby    |
 
     * el> baby spank
-      | ✨ el | waaahhhhh     |
-      | ✨ el | screaming cry |
-      | ✨ el | baby cry      |
-      | ✨ el | hurt          |
-      | ✨ el | wailing       |
+      | ✨ el | cry  |
+      | ✨ el | wail |
