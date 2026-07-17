80f3141d818ec6c42a50e75fb0b61d08a116ee19
msuarz <mikemps@gmail.com>
2026-07-17 00:13:47 -0400

    el: restore blank lines, extract route module

--- apps/el/lib/el/repl.ex
+++ apps/el/lib/el/repl.ex
@@ -1,19 +1,18 @@
 defmodule El.REPL do
   import Application, only: [ensure_all_started: 1]
   import Elita, only: [spawn: 2]
-  import El.Distribution, only: [wait: 1]
   import El.Tunnel, only: [boot: 1, reach: 1]
-  import El.Puppet, only: [ask: 2]
   import Agent.Harness, only: [dispatch: 3]
   import El.Sessions, only: [log: 1]
+  import El.Repl.Route, only: [route: 4]
   import IO, only: [read: 2, puts: 1, write: 1]
   import String, only: [trim: 1, split: 3]
-  import Utils.World, only: [agents: 0]
 
   def run(agent) do
     ensure_all_started(:elita)
     loop(agent, init(agent))
   end
+
   defp init(agent) do
     reg() |> settle()
     tape() |> settle()
@@ -25,10 +24,12 @@ defmodule El.REPL do
     import Registry, only: [start_link: 1]
     start_link(keys: :unique, name: ElitaRegistry)
   end
+
   defp tape do
     import Tape.Writer, only: [start_link: 1]
     start_link(nil)
   end
+
   defp find(agent) do
     agent |> reach() |> pick(agent)
   rescue
@@ -36,8 +37,11 @@ defmodule El.REPL do
   end
 
   defp pick(nil, agent), do: native(agent)
+
   defp pick(:undefined, agent), do: native(agent)
+
   defp pick(pid, _agent), do: pid
+
   defp native(agent) do
     import Elita, only: [spawn: 2]
     {:ok, pid} = spawn(agent, [agent])
@@ -46,51 +50,40 @@ defmodule El.REPL do
   end
 
   defp settle({:ok, _pid}), do: :ok
+
   defp settle({:error, {:already_started, _pid}}), do: :ok
+
   defp settle({:error, _}), do: :ok
+
   defp loop(agent, puppet) do
     write("#{agent}> ")
     read(:stdio, :line) |> next(agent, puppet)
   end
 
   defp next(:eof, _agent, _puppet), do: :ok
+
   defp next(line, agent, puppet) do
     handle(agent, puppet, trim(line)) |> proceed(agent, puppet)
   end
 
   defp proceed(:stop, _agent, _puppet), do: :ok
+
   defp proceed(:ok, agent, puppet), do: loop(agent, puppet)
+
   defp handle(_agent, _puppet, ""), do: :ok
+
   defp handle(_agent, _puppet, "/exit"), do: :stop
+
   defp handle(agent, puppet, input) when is_pid(puppet) do
     input |> split(" ", parts: 2) |> route(agent, puppet, input)
   end
 
   defp handle(agent, nil, "log"), do: agent |> log() |> puts()
+
   defp handle(agent, nil, input), do: dispatch(agent, input, :ask) |> puts()
+
   defp handle(agent, :undefined, "log"), do: agent |> log() |> puts()
+
   defp handle(agent, :undefined, input), do: dispatch(agent, input, :ask) |> puts()
-  defp route([name, "log"], _a, _p, _i), do: name |> log() |> puts()
-  defp route(_x, agent, puppet, input) do
-    words = input |> split(" ", parts: 2)
-    {response, target, pid} = via(words, puppet, input, agent)
-    puts(response)
-    result(target, agent, pid)
-  end
 
-  defp result(_target, _agent, _pid), do: :ok
-  defp via([_w], p, i, agent), do: {ask(p, i), agent, p}
-  defp via([w, msg], p, _i, agent), do: send(w, msg, p, agent, w in agents())
-  defp via(_, p, i, agent), do: {ask(p, i), agent, p}
-  defp send(w, msg, p, _agent, true) do
-    pid = choose(wait(w), p)
-    {ask(pid, msg), w, pid}
-  end
-  defp send(w, msg, p, agent, false) do
-    {ask(p, w <> " " <> msg), agent, p}
-  end
-
-  defp choose(nil, default), do: default
-  defp choose(:undefined, default), do: default
-  defp choose(t, _), do: t
 end
---
+++ apps/el/lib/el/repl/route.ex
@@ -0,0 +1,40 @@
+defmodule El.Repl.Route do
+  import El.Puppet, only: [ask: 2]
+  import El.Sessions, only: [log: 1]
+  import IO, only: [puts: 1]
+  import String, only: [split: 3]
+  import Utils.World, only: [agents: 0]
+  import El.Distribution, only: [wait: 1]
+
+  def route([name, "log"], _a, _p, _i), do: name |> log() |> puts()
+
+  def route(_x, agent, puppet, input) do
+    words = input |> split(" ", parts: 2)
+    {response, target, pid} = via(words, puppet, input, agent)
+    puts(response)
+    result(target, agent, pid)
+  end
+
+  def result(_target, _agent, _pid), do: :ok
+
+  def via([_w], p, i, agent), do: {ask(p, i), agent, p}
+
+  def via([w, msg], p, _i, agent), do: send(w, msg, p, agent, w in agents())
+
+  def via(_, p, i, agent), do: {ask(p, i), agent, p}
+
+  def send(w, msg, p, _agent, true) do
+    pid = choose(wait(w), p)
+    {ask(pid, msg), w, pid}
+  end
+
+  def send(w, msg, p, agent, false) do
+    {ask(p, w <> " " <> msg), agent, p}
+  end
+
+  def choose(nil, default), do: default
+
+  def choose(:undefined, default), do: default
+
+  def choose(t, _), do: t
+end
