14db48033be3a0163dbd3a5dcb12c6767cd286c7
msuarz <mikemps@gmail.com>
2026-07-20 22:45:54 -0400

    elita supervises tape writer

--- apps/elita/lib/application.ex
+++ apps/elita/lib/application.ex
@@ -6,6 +6,7 @@ defmodule Elita.Application do
   import Mem, only: [setup: 0]
   import Registry, only: [child_spec: 1]
   import Supervisor, only: [start_link: 2]
+  import System, only: [get_env: 1]
 
   def start(_type, _args) do
     setup()
@@ -23,14 +24,23 @@ defmodule Elita.Application do
     start_link(specs(), opts())
   end
 
-  defp specs do
-    [
-      child_spec(keys: :unique, name: ElitaRegistry),
-      {DynamicSupervisor, name: Elita.Spawner, strategy: :one_for_one}
-    ]
-  end
+  defp specs,
+    do:
+      [child_spec(keys: :unique, name: ElitaRegistry), spawner()] ++
+        tapes()
 
-  defp opts do
-    [strategy: :one_for_one, name: Elita.Supervisor]
-  end
+  defp tapes,
+    do: wrap(get_env("TAPE"))
+
+  defp wrap(nil), do: []
+  defp wrap(_), do: [tape()]
+
+  defp tape,
+    do: %{id: Tape.Writer, start: {Tape.Writer, :start_link, [nil]}}
+
+  defp spawner,
+    do: {DynamicSupervisor, name: Elita.Spawner, strategy: :one_for_one}
+
+  defp opts,
+    do: [strategy: :one_for_one, name: Elita.Supervisor]
 end
