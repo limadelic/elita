ff66943e03d95f2a1c1276fd27fe3d43ad5bae21
msuarz <mikemps@gmail.com>
2026-07-22 18:22:16 -0400

    clockwatcher xunit loops times of day

--- apps/elita/test/test_helper.exs
+++ apps/elita/test/test_helper.exs
@@ -1,3 +1,3 @@
 Code.require_file("support/tester.exs", __DIR__)
 Application.ensure_all_started(:tape)
-ExUnit.start(exclude: [:integration])
+ExUnit.start(exclude: [:integration, :rec])
--- apps/elita/test/xunit/clockwatcher_test.exs
+++ apps/elita/test/xunit/clockwatcher_test.exs
@@ -2,12 +2,32 @@ defmodule ClockwatcherTest do
   use Tester
   @moduletag :xunit
 
-  setup _context do
-    spawn(:clockwatcher)
-    :ok
-  end
+  @cases [
+    {"2025-07-07 06:00:00", "early", "don't start until 9"},
+    {"2025-07-07 10:00:00", "clockwatcher", "yes, I can help"},
+    {"2025-07-07 12:30:00", "lunch", "lunch"},
+    {"2025-07-07 18:00:00", "late", "done for the day"},
+    {"2025-07-12 10:00:00", "weekend", "come back Monday"}
+  ]
 
-  test "clockwatcher respects work hours" do
-    verify("10:00", ask(:clockwatcher, "can you handle this task?"))
+  for {time, cassette, fragment} <- @cases do
+    if cassette == "clockwatcher" do
+      @tag cassette: cassette
+      test "#{cassette}" do
+        System.put_env("CLOCK", unquote(time))
+        spawn(:clockwatcher)
+        result = ask(:clockwatcher, "can you handle this task?")
+        verify(unquote(fragment), result)
+      end
+    else
+      @tag cassette: cassette
+      @tag :rec
+      test "#{cassette}" do
+        System.put_env("CLOCK", unquote(time))
+        spawn(:clockwatcher)
+        result = ask(:clockwatcher, "can you handle this task?")
+        verify(unquote(fragment), result)
+      end
+    end
   end
 end
