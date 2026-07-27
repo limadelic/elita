9ba612fcc29769466032b64c5e55b964e823c631
msuarz <mikemps@gmail.com>
2026-07-27 19:37:10 -0400

    Move EL_TRACE env read to config layer

--- apps/matrix/lib/matrix/trace/log.ex
+++ apps/matrix/lib/matrix/trace/log.ex
@@ -1,21 +1,22 @@
 defmodule Matrix.Trace.Log do
   @moduledoc false
+  import Application
   import System
   import File
   import Matrix.Trace.Format
 
-  def chunk(data), do: jot(get_env("EL_TRACE"), trace(data))
+  def chunk(data), do: jot(get_env(:matrix, :trace_log), trace(data))
 
   def header({rows, cols}, source) do
-    jot(get_env("EL_TRACE"), caption(rows, cols, source))
+    jot(get_env(:matrix, :trace_log), caption(rows, cols, source))
   end
 
   def event(msg) do
-    jot(get_env("EL_TRACE"), stamp(msg))
+    jot(get_env(:matrix, :trace_log), stamp(msg))
   end
 
   def event(msg, reason) do
-    jot(get_env("EL_TRACE"), remark(msg, reason))
+    jot(get_env(:matrix, :trace_log), remark(msg, reason))
   end
 
   defp caption(rows, cols, source) do
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,1 +1,3 @@
 import Config
+
+config :matrix, :trace_log, System.get_env("EL_TRACE")
