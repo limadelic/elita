dc90a124ab5d6ed2435de5ac5c92d531291f7d0a
msuarz <mikemps@gmail.com>
2026-07-27 23:16:15 -0400

    Add releases config for umbrella, remove unnecessary app definition

--- mix.exs
+++ mix.exs
@@ -3,14 +3,18 @@ defmodule Elita.Umbrella do
 
   def project do
     [
-      app: :elita_umbrella,
       version: "0.0.2",
       elixir: "~> 1.18",
       apps_path: "apps",
       config_path: "config/config.exs",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        elita: [
+          applications: [el: :permanent, elita: :permanent, matrix: :permanent, tape: :permanent]
+        ]
+      ]
     ]
   end
 
