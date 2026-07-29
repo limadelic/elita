6b2d381f2e2bcd23741d43c6d4ef0997fbb7da7d
msuarz <mikemps@gmail.com>
2026-07-29 19:11:23 -0400

    Agent tree supervisor inert

---
+++ apps/elita/lib/agent/tree.ex
@@ -0,0 +1,24 @@
+defmodule Agent.Tree do
+  import Supervisor, only: [start_link: 2]
+
+  def start_link({name, configs, opts, via}) do
+    start_link([session(name, configs, opts, via)], opts())
+  end
+
+  defp session(name, configs, opts, via),
+    do: %{
+      id: :session,
+      start: launch(name, configs, opts, via),
+      restart: :transient
+    }
+
+  defp launch(name, configs, opts, via),
+    do: {GenServer, :start_link, [Elita, {name, configs, opts}, [name: via]]}
+
+  defp opts,
+    do: [
+      strategy: :rest_for_one,
+      max_restarts: 3,
+      max_seconds: 30
+    ]
+end
