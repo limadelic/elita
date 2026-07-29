c06ca2f88216618b8275a0dbc8e927e674f3eb94
msuarz <mikemps@gmail.com>
2026-07-29 19:17:37 -0400

    Tree dies with session

--- apps/elita/lib/agent/tree.ex
+++ apps/elita/lib/agent/tree.ex
@@ -9,7 +9,8 @@ defmodule Agent.Tree do
     do: %{
       id: :session,
       start: launch(name, configs, opts, via),
-      restart: :transient
+      restart: :transient,
+      significant: true
     }
 
   defp launch(name, configs, opts, via),
@@ -19,6 +20,7 @@ defmodule Agent.Tree do
     do: [
       strategy: :rest_for_one,
       max_restarts: 3,
-      max_seconds: 30
+      max_seconds: 30,
+      auto_shutdown: :any_significant
     ]
 end
