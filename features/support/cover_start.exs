8bda1d1e2f022474db56114176801c535012ead9
msuarz <mikemps@gmail.com>
2026-07-31 20:35:01 -0400

    Fix cover guards to accept all success shapes

--- features/support/cover_start.exs
+++ features/support/cover_start.exs
@@ -13,8 +13,10 @@ end
 if Node.connect(node_name) do
   # Start coverage
   start_result = :erpc.call(node_name, :cover, :start, [], 5000)
-  unless start_result == :ok do
-    raise "cover.start failed: #{inspect(start_result)}"
+  case start_result do
+    {:ok, _} -> nil
+    {:error, {:already_started, _}} -> nil
+    _ -> raise "cover.start failed: #{inspect(start_result)}"
   end
 
   # Compile beam directories for coverage
@@ -27,8 +29,12 @@ if Node.connect(node_name) do
 
   Enum.each(ebin_dirs, fn dir ->
     result = :erpc.call(node_name, :cover, :compile_beam_directory, [String.to_charlist(dir)], 5000)
-    unless result == :ok or (is_list(result) and length(result) > 0) do
-      raise "cover.compile_beam_directory failed for #{dir}: #{inspect(result)}"
+    valid = is_list(result) and
+            length(result) > 0 and
+            Enum.all?(result, &match?({:ok, _}, &1))
+    unless valid do
+      errors = Enum.filter(result, &match?({:error, _}, &1))
+      raise "cover.compile_beam_directory failed for #{dir}: #{inspect(errors)}"
     end
   end)
 end
