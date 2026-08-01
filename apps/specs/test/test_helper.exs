24608d3ac431520c9f7b7402be95496ce3822c6d
msuarz <mikemps@gmail.com>
2026-08-01 15:08:38 -0400

    Instrument coverage before app boot.

--- apps/specs/test/test_helper.exs
+++ apps/specs/test/test_helper.exs
@@ -1,5 +1,11 @@
 ExUnit.start()
 
+Application.stop(:elita)
+Application.stop(:matrix)
+
 Specs.Cover.start()
+
+Application.ensure_all_started(:matrix)
+Application.ensure_all_started(:elita)
 
 Code.require_file("support/helper.exs", __DIR__)
