6cf23658e286695c3ddc1e2d32efd624017bbc0f
msuarz <mikemps@gmail.com>
2026-08-01 12:28:40 -0400

    Restore stopd.exs to plain init.stop, remove coverage export

--- features/support/stopd.exs
+++ features/support/stopd.exs
@@ -9,26 +9,8 @@ end
 
 case Node.connect(node_name) do
   true ->
-    # Export coverage before shutdown
-    {:ok, build_root} = File.cwd()
-    coverage_dir = Path.join(build_root, "_build/test/coverage")
-    File.mkdir_p(coverage_dir)
-    edat_path = Path.join(coverage_dir, "cukes.edat")
-
-    export_error = nil
-    try do
-      :erpc.call(node_name, :cover, :export, [String.to_charlist(edat_path)], 5000)
-    rescue error ->
-      export_error = error
-    end
-
     :erpc.cast(node_name, :init, :stop, [])
     Process.sleep(500)
-
-    if export_error do
-      raise "Failed to export coverage: #{inspect(export_error)}"
-    end
-
     System.halt(0)
   false ->
     # Node not running, nothing to stop
