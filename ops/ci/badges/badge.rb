2f701e2e1733d5ad37f933350c42ec989925b5f1
msuarz <mikemps@gmail.com>
2026-09-22 16:33:05 -0400

    ci: add each missing badge link

--- ops/ci/badges/badge.rb
+++ ops/ci/badges/badge.rb
@@ -1,14 +1,27 @@
 module Badge
   def self.append(content)
-    content + block
+    content = content.dup
+    badges.each do |name|
+      content = append_if_missing(content, name)
+    end
+    content
   end
 
-  def self.block
-    "<!-- badges-start -->\n" \
-    "#{lint}\n" \
-    "#{cukes}\n" \
-    "#{cover}\n" \
-    "<!-- badges-end -->\n"
+  def self.badges
+    %w[lint cukes cover greet]
+  end
+
+  def self.append_if_missing(content, badge_name)
+    link = send(badge_name)
+    return content if content.include?(link)
+
+    append_link(content, link)
+  end
+
+  def self.append_link(content, link)
+    normalized = content.sub(/\n+\z/, '')
+    padding = normalized.empty? ? '' : "\n"
+    normalized + padding + link + "\n"
   end
 
   def self.lint
@@ -26,5 +39,10 @@ module Badge
     pr = ENV.fetch('PR_NUMBER')
     url = ENV.fetch('COVER_URL')
     "[![cover](https://img.shields.io/endpoint?url=#{url})](https://limadelic.github.io/elita/#{pr}/cover/index.html)"
+  end
+
+  def self.greet
+    pr = ENV.fetch('PR_NUMBER')
+    "[![greet](https://img.shields.io/badge/greet-video-blue)](https://limadelic.github.io/elita/#{pr}/greet.mp4)"
   end
 end
