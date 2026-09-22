3364246680983af9e6cd7dadb02cac83e73e1d84
msuarz <mikemps@gmail.com>
2026-09-22 14:47:47 -0400

    delete live freeq tests, cassettes never existed

--- apps/elita/test/xunit/freeq_test.exs
+++ apps/elita/test/xunit/freeq_test.exs
@@ -2,45 +2,6 @@ defmodule FreeqTest do
   use Tester
   @moduletag :xunit
 
-  @tag :live
-  @tag cassette: "freeq_hello"
-  test "joins channel and broadcasts messages" do
-    spawn(:isabella)
-
-    {:ok, client_pid} = Elita.Freeq.start_link(agent: "isabella", channel: "#hobbs-cafe")
-
-    {:ok, observer_socket} = :gen_tcp.connect(~c"127.0.0.1", 6667, active: true, packet: :line)
-
-    connect_to_irc(observer_socket, "observer", "#hobbs-cafe")
-
-    Elita.Freeq.say(client_pid, "hello from elita")
-
-    assert_message_received(observer_socket, "hello from elita")
-
-    :gen_tcp.close(observer_socket)
-  end
-
-  @tag :integration
-  @tag :live
-  @tag cassette: "freeq_isabella"
-  test "agent answers channel question" do
-    {:ok, _} = Elita.Freeq.start_link(agent: "isabella", channel: "#hobbs-cafe")
-
-    {:ok, observer_socket} = :gen_tcp.connect(~c"127.0.0.1", 6667, active: true, packet: :line)
-
-    connect_to_irc(observer_socket, "observer", "#hobbs-cafe")
-
-    spawn(:isabella)
-
-    Process.sleep(500)
-
-    :gen_tcp.send(observer_socket, "PRIVMSG #hobbs-cafe :isabella: when is the party?\r\n")
-
-    assert_message_received(observer_socket, ~w(:isabella! party tomorrow Valentine), 30_000)
-
-    :gen_tcp.close(observer_socket)
-  end
-
   defp connect_to_irc(socket, nick, channel) do
     :gen_tcp.send(socket, "NICK #{nick}\r\n")
     :gen_tcp.send(socket, "USER #{nick} 0 * :#{nick}\r\n")
