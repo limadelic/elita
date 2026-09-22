de74183ab160100edacd2a59f6ca10644a04c4e9
msuarz <mikemps@gmail.com>
2026-09-22 16:36:30 -0400

    ci: greet video in badge rebuild set

--- ops/ci/badges/badge.rb
+++ ops/ci/badges/badge.rb
@@ -1,27 +1,15 @@
 module Badge
   def self.append(content)
-    content = content.dup
-    badges.each do |name|
-      content = append_if_missing(content, name)
-    end
-    content
+    content + block
   end
 
-  def self.badges
-    %w[lint cukes cover greet]
-  end
-
-  def self.append_if_missing(content, badge_name)
-    link = send(badge_name)
-    return content if content.include?(link)
-
-    append_link(content, link)
-  end
-
-  def self.append_link(content, link)
-    normalized = content.sub(/\n+\z/, '')
-    padding = normalized.empty? ? '' : "\n"
-    normalized + padding + link + "\n"
+  def self.block
+    "<!-- badges-start -->\n" \
+    "#{lint}\n" \
+    "#{cukes}\n" \
+    "#{cover}\n" \
+    "#{greet}\n" \
+    "<!-- badges-end -->\n"
   end
 
   def self.lint
