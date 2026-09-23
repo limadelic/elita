e339bdb1d441fabb9b0085e7d767ee8d684e0a6a
msuarz <mikemps@gmail.com>
2026-09-22 20:36:56 -0400

    port second boss test to freeq

--- apps/elita/test/freeq/boss_test.exs
+++ apps/elita/test/freeq/boss_test.exs
@@ -59,4 +59,78 @@ defmodule FreeqBossTest do
     GenServer.stop(dev_pid)
     GenServer.stop(qa_pid)
   end
+
+  @tag cassette: "boss2"
+  test "michael asks dwight to photocopy sales reports" do
+    Server.wait(~c"127.0.0.1", 6667)
+    {:ok, socket} = FreeqTestClient.connect()
+    {:ok, counter} = Agent.start_link(fn -> 1 end)
+
+    Tester.spawn(:michael, :boss)
+    Tester.spawn(:dwight, :boss)
+    Tester.spawn(:pam, :worker)
+    Tester.spawn(:jim, :worker)
+
+    FreeqTestClient.register_brian(socket, counter)
+
+    {:ok, michael_pid} =
+      Elita.Freeq.start_link(agent: "michael", channel: "#the-lab", driver: "brian")
+
+    {:ok, dwight_pid} =
+      Elita.Freeq.start_link(agent: "dwight", channel: "#the-lab", driver: "brian")
+
+    {:ok, pam_pid} = Elita.Freeq.start_link(agent: "pam", channel: "#the-lab", driver: "brian")
+    {:ok, jim_pid} = Elita.Freeq.start_link(agent: "jim", channel: "#the-lab", driver: "brian")
+
+    FreeqTestClient.wait_join(socket, "michael", counter)
+    FreeqTestClient.wait_join(socket, "dwight", counter)
+    FreeqTestClient.wait_join(socket, "pam", counter)
+    FreeqTestClient.wait_join(socket, "jim", counter)
+
+    FreeqTestClient.send_turn(
+      socket,
+      "#the-lab",
+      "michael: you manage dwight the assistant regional manager",
+      counter
+    )
+
+    FreeqTestClient.wait_fragment(socket, "understand", counter)
+
+    FreeqTestClient.send_turn(
+      socket,
+      "#the-lab",
+      "dwight: you manage pam the receptionist and jim the salesman",
+      counter
+    )
+
+    FreeqTestClient.wait_fragment(socket, "understand", counter)
+
+    FreeqTestClient.send_turn(
+      socket,
+      "#the-lab",
+      "michael: we need 50 copies of the quarterly sales report",
+      counter
+    )
+
+    FreeqTestClient.wait_fragment(socket, "done", counter)
+
+    FreeqTestClient.send_turn(socket, "#the-lab", "jim: did you receive a task?", counter)
+
+    FreeqTestClient.wait_fragment(socket, "no", counter)
+
+    FreeqTestClient.send_turn(
+      socket,
+      "#the-lab",
+      "pam: did you receive a task to make copies?",
+      counter
+    )
+
+    FreeqTestClient.wait_fragment(socket, "yes", counter)
+
+    :gen_tcp.close(socket)
+    GenServer.stop(michael_pid)
+    GenServer.stop(dwight_pid)
+    GenServer.stop(pam_pid)
+    GenServer.stop(jim_pid)
+  end
 end
