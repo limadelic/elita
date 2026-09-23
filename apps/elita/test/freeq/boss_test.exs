a2b1188c8bf377d2d3851903b2df78a2c536b0f1
msuarz <mikemps@gmail.com>
2026-09-22 23:24:47 -0400

    freeq boss matches its twin

--- apps/elita/test/freeq/boss_test.exs
+++ apps/elita/test/freeq/boss_test.exs
@@ -5,51 +5,25 @@ defmodule FreeqBossTest do
 
   @tag cassette: "boss"
   test "boss delegates in the lab" do
-    join :boss
-    join :dev, :worker
-    join :qa, :worker
-
-    say "boss: you manage a software development team with a dev and a qa"
-
-    hears "ready"
-
-    say "boss: we need more test created"
-    hears "done"
-
-    say "dev: did you receive a task from boss?"
-
-    hears "no"
-
-    say "qa: did you receive a task from boss?"
-
-    hears "yes"
+    spawn(:boss)
+    spawn(:dev, :worker)
+    spawn(:qa, :worker)
+    tell(:boss, "you manage a software development team with a dev and a qa")
+    verify("done", ask(:boss, "we need more test created"))
+    verify("no", ask(:dev, "did you receive a task from boss?"))
+    verify("yes", ask(:qa, "did you receive a task from boss?"))
   end
 
   @tag cassette: "boss2"
   test "michael asks dwight to photocopy sales reports" do
-    join :michael, :boss
-    join :dwight, :boss
-    join :pam, :worker
-    join :jim, :worker
-
-    say "michael: you manage dwight the assistant regional manager"
-
-    hears "understand"
-
-    say "dwight: you manage pam the receptionist and jim the salesman"
-
-    hears "understand"
-
-    say "michael: we need 50 copies of the quarterly sales report"
-
-    hears "done"
-
-    say "jim: did you receive a task?"
-
-    hears "no"
-
-    say "pam: did you receive a task to make copies?"
-
-    hears "yes"
+    spawn(:michael, :boss)
+    spawn(:dwight, :boss)
+    spawn(:pam, :worker)
+    spawn(:jim, :worker)
+    tell(:michael, "you manage dwight the assistant regional manager")
+    tell(:dwight, "you manage pam the receptionist and jim the salesman")
+    verify("done", ask(:michael, "we need 50 copies of the quarterly sales report"))
+    verify("no", ask(:jim, "did you receive a task?"))
+    verify("yes", ask(:pam, "did you receive a task to make copies?"))
   end
 end
