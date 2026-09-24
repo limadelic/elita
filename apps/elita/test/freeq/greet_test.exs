28f10f23e6535ca115f8b2dcc7fd6e95aade0e67
msuarz <mikemps@gmail.com>
2026-09-23 21:20:50 -0400

    Import module functions in greet_test.exs

--- apps/elita/test/freeq/greet_test.exs
+++ apps/elita/test/freeq/greet_test.exs
@@ -1,8 +1,13 @@
 defmodule Freeq.GreetTest do
   use ExUnit.Case
 
+  import :gen_tcp, only: [connect: 3, recv: 3]
+  import Regex, only: [compile!: 1, escape: 1]
+  import String, only: [trim: 1, split: 2]
+  import List, only: [last: 1]
+
   setup context do
-    {:ok, sock} = :gen_tcp.connect(~c"127.0.0.1", 6667, packet: :line, active: false)
+    {:ok, sock} = connect(~c"127.0.0.1", 6667, packet: :line, active: false)
     login(sock)
     join(sock)
     greet(sock, word(context.test))
@@ -28,7 +33,7 @@ defmodule Freeq.GreetTest do
 
   defp greet(sock, word) do
     :gen_tcp.send(sock, "PRIVMSG #the-lab :#{word}\r\n")
-    pattern = Regex.compile!("^:brian!\\S+ PRIVMSG #the-lab :#{Regex.escape(word)}\\r?$")
+    pattern = compile!("^:brian!\\S+ PRIVMSG #the-lab :#{escape(word)}\\r?$")
     expect(sock, pattern)
   end
 
@@ -41,12 +46,12 @@ defmodule Freeq.GreetTest do
   end
 
   defp scan(sock, regex) do
-    {:ok, data} = :gen_tcp.recv(sock, 0, 5000)
-    scan(sock, regex, to_string(data))
+    {:ok, data} = recv(sock, 0, 5000)
+    scan(sock, regex, Kernel.to_string(data))
   end
 
   defp scan(sock, regex, "PING " <> server) do
-    :gen_tcp.send(sock, "PONG #{String.trim(server)}\r\n")
+    :gen_tcp.send(sock, "PONG #{trim(server)}\r\n")
     scan(sock, regex)
   end
 
@@ -64,6 +69,6 @@ defmodule Freeq.GreetTest do
   end
 
   defp word(name) do
-    name |> Atom.to_string() |> String.split(~r/[ _]/) |> List.last()
+    name |> Atom.to_string() |> split(~r/[ _]/) |> last()
   end
 end
