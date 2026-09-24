6e0198091a4dc51ce79b991da818a09505659cec
msuarz <mikemps@gmail.com>
2026-09-23 20:34:50 -0400

    Rewrite greet test with IRC handshake

--- apps/elita/test/freeq/greet_test.exs
+++ apps/elita/test/freeq/greet_test.exs
@@ -2,8 +2,20 @@ defmodule Freeq.GreetTest do
   use ExUnit.Case
 
   setup context do
-    {:ok, _sock} = :gen_tcp.connect(~c"127.0.0.1", 6667, active: false)
-    {:ok, []}
+    {:ok, sock} = :gen_tcp.connect(~c"127.0.0.1", 6667, packet: :line, active: false)
+    send_cap(sock)
+    send_nick(sock)
+    send_user(sock)
+    send_cap_end(sock)
+    _ = read_until(sock, "001")
+    send_join(sock)
+    line = read_until(sock, "JOIN")
+    assert String.contains?(line, "brian")
+    word = extract_word(context.test)
+    send_privmsg(sock, word)
+    line = read_until(sock, "PRIVMSG")
+    assert String.contains?(line, "brian")
+    {:ok, socket: sock}
   end
 
   test "greet" do
@@ -11,30 +23,54 @@ defmodule Freeq.GreetTest do
   end
 
   defp send_cap(sock) do
-    :ok = :gen_tcp.send(sock, "CAP REQ :echo-message\r\n")
+    :gen_tcp.send(sock, "CAP REQ :echo-message\r\n")
   end
 
   defp send_nick(sock) do
-    :ok = :gen_tcp.send(sock, "NICK brian\r\n")
+    :gen_tcp.send(sock, "NICK brian\r\n")
   end
 
   defp send_user(sock) do
-    :ok = :gen_tcp.send(sock, "USER brian 0 * :brian\r\n")
+    :gen_tcp.send(sock, "USER brian 0 * :brian\r\n")
+  end
+
+  defp send_cap_end(sock) do
+    :gen_tcp.send(sock, "CAP END\r\n")
   end
 
   defp send_join(sock) do
-    :ok = :gen_tcp.send(sock, "JOIN #the-lab\r\n")
+    :gen_tcp.send(sock, "JOIN #the-lab\r\n")
   end
 
-  defp send_msg(sock, word) do
-    :ok = :gen_tcp.send(sock, "PRIVMSG #the-lab :#{word}\r\n")
+  defp send_privmsg(sock, word) do
+    :gen_tcp.send(sock, "PRIVMSG #the-lab :#{word}\r\n")
   end
 
-  defp read_from(sock) do
-    {:ok, _} = :gen_tcp.recv(sock, 4096, 5000)
+  defp read_until(sock, marker) do
+    {:ok, data} = :gen_tcp.recv(sock, 0, 5000)
+    check_line(sock, to_string(data), marker)
+  end
+
+  defp check_line(sock, line, marker) do
+    if String.contains?(line, marker),
+      do: line,
+      else:
+        (
+          handle_ping(sock, line)
+          read_until(sock, marker)
+        )
+  end
+
+  defp handle_ping(sock, line) do
+    String.starts_with?(line, "PING") && send_pong(sock, line)
+  end
+
+  defp send_pong(sock, line) do
+    [_, server] = String.split(line, " ", parts: 2)
+    :gen_tcp.send(sock, "PONG #{String.trim(server)}\r\n")
   end
 
   defp extract_word(name) do
-    name |> Atom.to_string() |> String.trim_prefix("test_")
+    name |> Atom.to_string() |> String.split(~r/[ _]/) |> List.last()
   end
 end
